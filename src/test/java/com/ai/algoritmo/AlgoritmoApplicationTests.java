0ae43390575bf831e6da99d3f0b7c3e439ed0671
EukziN <ehpereira@minha.fag.edu.br>
2026-09-15 20:51:14 -0300

    feat: iniciando projeto Spring API

---
+++ src/test/java/com/ai/algoritmo/AlgoritmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.algoritmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoritmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
